e42b5156c8e9b206e5a3ad4f64470cacbd4f3ba5
Tangjj1996 <awesometangji@gmail.com>
2024-08-21 19:39:03 +0800

    feat: 联接 postgres 数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '1234',
+      database: 'nest-demo-1',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
