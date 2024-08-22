21867f42e28d9b6e6504fd329c6fd1164bbed72f
Tangjj1996 <awesometangji@gmail.com>
2024-08-22 17:18:42 +0800

    feat: 支持环境变量动态加载

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { isProd } from '@/utils';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'tangji',
-      database: 'nestjs-demo-1',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: isProd
+        ? path.resolve(__dirname, '..', '.env.production.local')
+        : path.resolve(__dirname, '..', '.env'),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: isProd,
+      }),
     }),
     UserModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 全局前缀
+  app.setGlobalPrefix('api');
   // 使用全局验证管道
   app.useGlobalPipes(new ValidationPipe());
 
