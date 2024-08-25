01410ca659ce8baadb40126a4dc29b498516cddb
Tangjj1996 <awesometangji@gmail.com>
2024-08-25 15:04:16 +0800

    feat: 魔鬼数字清理

--- src/core/filter/http-exception/http-exception.filter.ts
+++ src/core/filter/http-exception/http-exception.filter.ts
@@ -3,6 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 
 @Catch()
@@ -13,15 +14,16 @@ export class HttpExceptionFilter<T extends HttpException>
     const ctx = host.switchToHttp(); // 获取请求上下文
     const response = ctx.getResponse(); // 获取请求上下文中的 response 对象
     // todo dto 转换抛出错误
-    const status = exception?.getStatus?.() || 500; // 获取异常状态码
+    const status = exception?.getStatus?.() || HttpStatus.INTERNAL_SERVER_ERROR; // 获取异常状态码
 
     // 设置错误信息
     const message =
-      exception.message || `${status > 500 ? 'Service error' : 'Client Error'}`;
+      exception.message ||
+      `${status > HttpStatus.INTERNAL_SERVER_ERROR ? 'Service error' : 'Client Error'}`;
 
     const errorResponse = {
       data: null,
-      code: response?.code ?? -1,
+      code: response?.code ?? HttpStatus.INTERNAL_SERVER_ERROR,
       msg: response?.msg ?? message,
       traceId: '111',
     };
