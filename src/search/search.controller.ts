c28954cec50172ef1a94d9f2732dbe6d236aa311
Tangjj1996 <awesometangji@gmail.com>
2024-08-31 11:18:38 +0800

    feat: 构建报错优化

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,43 +1,8 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
-import { CreateSearchDto } from './dto/create-search.dto';
-import { UpdateSearchDto } from './dto/update-search.dto';
 import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
-
-  @Post()
-  create(@Body() createSearchDto: CreateSearchDto) {
-    return this.searchService.create(createSearchDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.searchService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.searchService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSearchDto: UpdateSearchDto) {
-    return this.searchService.update(+id, updateSearchDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.searchService.remove(+id);
-  }
 }
