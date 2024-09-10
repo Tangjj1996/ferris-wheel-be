d98cfe9bce35ae655c210052181caf5ccaeffdd3
Tangjj1996 <awesometangji@gmail.com>
2024-09-10 10:46:43 +0800

    feat: 实体仓库注入

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from '@/user/user.module';
 
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
+import { HotDashboardConfig } from './entities/HotDashboardConfig';
+import { HotDashboardConfigItems } from './entities/HotDashboardConfigItems';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forFeature([HotDashboardConfig, HotDashboardConfigItems]),
+    UserModule,
+  ],
   controllers: [CommonController],
   providers: [CommonService],
 })
